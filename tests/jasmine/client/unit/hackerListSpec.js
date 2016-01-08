33b3232858bfe3299d550b03ca6a4fe393db3071
pmoons <peter.mooney90@gmail.com>
2016-01-07 22:56:39 -0500

    Make dummy test

---
+++ tests/jasmine/client/unit/hackerListSpec.js
@@ -0,0 +1,8 @@
+describe('HackerListService', function() {
+
+	describe('getHackerData', function() {
+		it('should return a cursor of all the Hackers in the database', function() {
+			expect(true).toBe(true);
+		});
+	});
+});
